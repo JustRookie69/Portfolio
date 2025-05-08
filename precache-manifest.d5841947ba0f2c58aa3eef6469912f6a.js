cdaf9f04308e8ca54c7243d7c75cc2005c510ba0
Shivansh <shivanshy60@gmail.com>
2025-05-08 17:52:11 +0530

    Updates

---
+++ precache-manifest.d5841947ba0f2c58aa3eef6469912f6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac79f0f608acc533b311fcad2208ed2b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "4f30e1c140b19b97e641",
+    "url": "/Portfolio/static/css/main.f7cd265e.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/Portfolio/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/Portfolio/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "4f30e1c140b19b97e641",
+    "url": "/Portfolio/static/js/main.5f6579d6.chunk.js"
+  },
+  {
+    "revision": "91a146410ea4bb6dee85",
+    "url": "/Portfolio/static/js/runtime~main.1ee3f3bf.js"
+  }
+]);
